6015fd193184bea0fb1a0f9d895f1f35a67f35b6
tsvetkov <tsvetk0ff@ya.ru>
2017-12-27 17:57:43 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="T-chat",
+    version='0.1.1',
+    description='Simple messenger.',
+    long_description='T-chat is a simple messenger with private chats and chat-rooms.',
+    url='https://github.com/tsvetk0ff/MessengerProject',
+    license='MIT',
+    keywords=['python', 'messenger', 'T-chat'],
+    author='Igor Tsvetkov',
+    author_email='tsvetkov.igor.n@gmail.com',
+    # packages=['hanita', 'hanita/forms', 'hanita_server', 'hanita_JIM'],
+    # include_package_data=True,
+    # python_requires='>=3.5',
+    # install_requires=[
+    #     'PyQt5==5.9', 'SQLAlchemy==1.1.15'
+    ],
+)
