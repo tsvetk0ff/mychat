50822c849fa780a54110e821d577a7a9dd5c1c17
Igor <tsvetk0ff@ya.ru>
2018-01-15 22:42:09 +0300

    deleted not used files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="T-chat",
     version='0.1.1',
     description='Simple messenger.',
-    long_description='T-chat is a simple messenger with private chats and chat-rooms.',
+    long_description='T-chat is a simple GUI & console messenger for Windows.',
     url='https://github.com/tsvetk0ff/MessengerProject',
     license='MIT',
     keywords=['python', 'messenger', 'T-chat'],
